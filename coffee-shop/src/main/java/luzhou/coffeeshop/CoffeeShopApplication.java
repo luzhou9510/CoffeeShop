52c21be18d16a9f05ba23cd2b8c996fc62086cf5
Joe <1226937101@qq.com>
2020-11-03 23:11:01 +0800

    finished init order and find order method

--- coffee-shop/src/main/java/luzhou/coffeeshop/CoffeeShopApplication.java
+++ coffee-shop/src/main/java/luzhou/coffeeshop/CoffeeShopApplication.java
@@ -2,6 +2,8 @@ package luzhou.coffeeshop;
 
 import lombok.extern.slf4j.Slf4j;
 import luzhou.coffeeshop.model.Coffee;
+import luzhou.coffeeshop.model.CoffeeOrder;
+import luzhou.coffeeshop.model.OrderState;
 import luzhou.coffeeshop.repository.CoffeeOrderRepository;
 import luzhou.coffeeshop.repository.CoffeeRepository;
 import org.joda.money.CurrencyUnit;
@@ -11,10 +13,15 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -39,11 +46,57 @@ public class CoffeeShopApplication implements ApplicationRunner {
 	}
 
 	private void findOrders() {
+		coffeeRepository
+				.findAll(Sort.by(Sort.Direction.ASC, "name"))
+				.forEach(c -> log.info("Loading {}", c));
+
+		List<CoffeeOrder> list = coffeeOrderRepository.findTop3ByOrderByUpdateTimeDescIdAsc();
+		log.info("find Top 3 by Order by Update Time Desc Id Asc: {}", getJoinedOrderId(list));
+
+		list = coffeeOrderRepository.findByCustomerOrderById("Zhang San");
+		log.info("find by customer order by id: {}", getJoinedOrderId(list));
+
+		list.forEach(o -> {
+			log.info("Order {}", o.getId());
+			o.getItems().forEach(i -> log.info(" Item {}", i));
+		});
+
+		list = coffeeOrderRepository.findByItems_Name("latte");
+		log.info("find by items_name: {}", getJoinedOrderId(list));
 	}
 
 	private void initOrders() {
 		Coffee latte = Coffee.builder().name("latte")
 				.price(Money.of(CurrencyUnit.of("CNY"), 30.0))
 				.build();
+		coffeeRepository.save(latte);
+		log.info("Coffee: {}", latte);
+
+		Coffee espresso = Coffee.builder().name("espresso")
+						.price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+						.build();
+		coffeeRepository.save(espresso);
+		log.info("Coffee: {}", espresso);
+
+		CoffeeOrder order = CoffeeOrder.builder()
+						.customer("Zhang San")
+						.items(Collections.singletonList(espresso))
+						.state(OrderState.INIT)
+						.build();
+		coffeeOrderRepository.save(order);
+		log.info("Order: {}", order);
+
+		order = CoffeeOrder.builder()
+						.customer("Zhang San")
+						.items(Arrays.asList(espresso, latte))
+						.state(OrderState.INIT)
+						.build();
+		coffeeOrderRepository.save(order);
+		log.info("Order: {}", order);
+	}
+
+	private String getJoinedOrderId(List<CoffeeOrder> list) {
+		return list.stream().map(o -> o.getId().toString())
+				.collect(Collectors.joining(","));
 	}
 }
